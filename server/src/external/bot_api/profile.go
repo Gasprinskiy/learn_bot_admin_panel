9dae7dd69aa5015dbaa4131e239a4dbdacdcc3dd
Gasprinskiy <gasprin321@gmail.com>
2025-07-27 18:36:13 +0500

    feat: sse tg bot auth

---
+++ server/src/external/bot_api/profile.go
@@ -0,0 +1,63 @@
+package bot_api
+
+import (
+	"context"
+	"learn_bot_admin_panel/config"
+	"learn_bot_admin_panel/internal/entity/telegram"
+	"learn_bot_admin_panel/internal/transaction"
+	"learn_bot_admin_panel/tools/bot_tool"
+	"learn_bot_admin_panel/tools/logger"
+	"learn_bot_admin_panel/uimport"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+type BotProfileHandler struct {
+	ui     *uimport.UsecaseImport
+	b      *bot.Bot
+	config *config.Config
+	log    *logger.Logger
+	sm     transaction.SessionManager
+}
+
+func NewBotProfileHandler(
+	ui *uimport.UsecaseImport,
+	b *bot.Bot,
+	config *config.Config,
+	log *logger.Logger,
+	sm transaction.SessionManager,
+) {
+	handler := BotProfileHandler{
+		ui,
+		b,
+		config,
+		log,
+		sm,
+	}
+
+	handler.b.RegisterHandler(
+		bot.HandlerTypeMessageText,
+		"/start",
+		bot.MatchTypePrefix,
+		handler.TgAuthHandler,
+	)
+}
+
+func (h *BotProfileHandler) TgAuthHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	message, err := transaction.RunInTx(
+		ctx,
+		h.log,
+		h.sm,
+		func(ctx context.Context) (string, error) {
+			return h.ui.Usecase.Profile.TgAuthVerify(ctx, update.Message.From.Username, update.Message.Text)
+		},
+	)
+
+	if err != nil {
+		bot_tool.SendHTMLParseModeMessage(ctx, b, update, telegram.MessagesByError[err])
+		return
+	}
+
+	bot_tool.SendHTMLParseModeMessage(ctx, b, update, message)
+}
--- server/src/external/rest_api/profile.go
+++ server/src/external/rest_api/profile.go
@@ -1,49 +1,110 @@
-package restapi
+package rest_api
 
 import (
+	"fmt"
 	"learn_bot_admin_panel/config"
-	"learn_bot_admin_panel/internal/event_bus"
+	"learn_bot_admin_panel/internal/entity/global"
+
 	"learn_bot_admin_panel/uimport"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type ProfileHandler struct {
-	ui       *uimport.UsecaseImport
-	gin      *gin.Engine
-	config   *config.Config
-	uuidChan event_bus.UUIDChanel
+	ui     *uimport.UsecaseImport
+	gin    *gin.Engine
+	config *config.Config
 }
 
 func NewProfileHandler(
 	ui *uimport.UsecaseImport,
 	gin *gin.Engine,
 	config *config.Config,
-	uuidChan event_bus.UUIDChanel,
 ) {
 	handler := ProfileHandler{
 		ui,
 		gin,
 		config,
-		uuidChan,
 	}
 
-	handler.gin.Group("/auth")
+	group := handler.gin.Group("/auth")
 
 	{
-		handler.gin.GET(
-			"/temp_id",
-			GetTemporaryID,
+		group.GET(
+			"/temp_data",
+			handler.GetAuthData,
 		)
 
-		handler.gin.GET(
+		group.GET(
 			"/listen",
+			handler.HandleAuthListen,
 		)
 	}
 }
 
-func GetTemporaryID(ctx *gin.Context) {
-	// uuID := uuid.NewString()
+func (h *ProfileHandler) GetAuthData(gctx *gin.Context) {
+	authUrlResponse, err := h.ui.Usecase.Profile.CreateAuthUrlResponse()
+	if err != nil {
+		gctx.JSON(global.ErrStatusCodes[err], gin.H{"message": err.Error()})
+		return
+	}
 
-	// ctx.JSON(http.StatusCreated)
+	gctx.JSON(http.StatusOK, authUrlResponse)
 }
+
+func (h *ProfileHandler) HandleAuthListen(gctx *gin.Context) {
+	authKey := gctx.Query("auth_id")
+	if authKey == "" {
+		gctx.JSON(http.StatusBadRequest, gin.H{"message": global.ErrInvalidParam})
+		return
+	}
+
+	flusher, ok := gctx.Writer.(http.Flusher)
+	if !ok {
+		gctx.JSON(http.StatusInternalServerError, gin.H{"message": global.ErrInternalError})
+		return
+	}
+
+	gctx.Writer.Header().Set("Content-Type", "text/event-stream")
+	gctx.Writer.Header().Set("Cache-Control", "no-cache")
+	gctx.Writer.Header().Set("Connection", "keep-alive")
+
+	userData, err := h.ui.Usecase.Profile.WaitTgAuthVerify(gctx.Request.Context(), authKey)
+	if err != nil {
+		fmt.Fprintf(gctx.Writer, "event: error\ndata: %s\n\n", err.Error())
+		flusher.Flush()
+		return
+	}
+
+	fmt.Fprintf(gctx.Writer, "event: done\ndata: %s\n\n", userData)
+	flusher.Flush()
+}
+
+// authSession, exists := h.authChan.Read(authID)
+// if !exists {
+// 	gctx.JSON(http.StatusGone, gin.H{"message": global.ErrExpired})
+// 	return
+// }
+
+// defer h.authChan.CleanUp(authID)
+
+// select {
+// case <-gctx.Request.Context().Done():
+// 	return
+
+// case userData := <-authSession.Chan:
+// 	jsonData, err := json.Marshal(userData)
+// 	if err != nil {
+// 		gctx.JSON(http.StatusInternalServerError, gin.H{"message": global.ErrInternalError})
+// 		return
+// 	}
+
+// 	fmt.Fprintf(gctx.Writer, "event: done\ndata: %s\n\n", jsonData)
+// 	flusher.Flush()
+// 	return
+
+// case <-time.After(h.config.SSETTL):
+// 	gctx.JSON(http.StatusGone, gin.H{"message": global.ErrExpired})
+// 	return
+// }
