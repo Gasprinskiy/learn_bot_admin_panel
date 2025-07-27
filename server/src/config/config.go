7cbc59f97676e5097c44ce8026b9ba1e0d4eb22c
Gasprinskiy <gasprin321@gmail.com>
2025-07-27 18:35:37 +0500

    feat: temp config change

--- server/src/config/config.go
+++ server/src/config/config.go
@@ -11,6 +11,7 @@ import (
 // Config структура для хранения переменных окружения
 type Config struct {
 	PostgresURL  string
+	TgApiURL     string
 	BotToken     string
 	RedisAddr    string
 	RedisPass    string
@@ -28,25 +29,26 @@ func NewConfig() *Config {
 		log.Panic("не удалось получить время жизни кеша: ", err)
 	}
 
-	jwtSecretTtl, err := strconv.Atoi(os.Getenv("JWT_SECRET_TTL"))
-	if err != nil {
-		log.Panic("не удалось получить время жизни jwt токена: ", err)
-	}
+	// jwtSecretTtl, err := strconv.Atoi(os.Getenv("JWT_SECRET_TTL"))
+	// if err != nil {
+	// 	log.Panic("не удалось получить время жизни jwt токена: ", err)
+	// }
 
-	sseTtl, err := strconv.Atoi(os.Getenv("JWT_SECRET_TTL"))
+	sseTtl, err := strconv.Atoi(os.Getenv("SSE_TTL"))
 	if err != nil {
 		log.Panic("не удалось получить время жизни sse канала: ", err)
 	}
 
 	return &Config{
-		PostgresURL:  os.Getenv("POSTGRES_URL"),
-		BotToken:     os.Getenv("BOT_TOKEN"),
-		RedisPass:    os.Getenv("REDIS_PASSWORD"),
-		JwtSecret:    os.Getenv("JWT_SECRET"),
-		ServerPort:   fmt.Sprintf(":%s", os.Getenv("HTTP_SERVER_PORT")),
-		RedisAddr:    fmt.Sprintf("redis:%s", os.Getenv("REDIS_PORT")),
-		RedisTTL:     time.Minute * time.Duration(redisTtl),
-		JwtSecretTTL: time.Minute * time.Duration(jwtSecretTtl),
-		SSETTL:       time.Minute * time.Duration(sseTtl),
+		PostgresURL: os.Getenv("POSTGRES_URL"),
+		TgApiURL:    os.Getenv("TG_API_PATH"),
+		BotToken:    os.Getenv("BOT_TOKEN"),
+		RedisPass:   os.Getenv("REDIS_PASSWORD"),
+		// JwtSecret:   os.Getenv("JWT_SECRET"),
+		ServerPort: fmt.Sprintf(":%s", os.Getenv("HTTP_SERVER_PORT")),
+		RedisAddr:  fmt.Sprintf("redis:%s", os.Getenv("REDIS_PORT")),
+		RedisTTL:   time.Minute * time.Duration(redisTtl),
+		// JwtSecretTTL: time.Minute * time.Duration(jwtSecretTtl),
+		SSETTL: time.Minute * time.Duration(sseTtl),
 	}
 }
