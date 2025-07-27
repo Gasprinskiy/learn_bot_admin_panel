fdfaf0eba3cb0b037f080fa6de52872e8f482e87
Gasprinskiy <gasprin321@gmail.com>
2025-07-27 18:37:39 +0500

    feat: project init

--- server/src/main.go
+++ server/src/main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"learn_bot_admin_panel/config"
+	"learn_bot_admin_panel/external/bot_api"
+	"learn_bot_admin_panel/external/rest_api"
+	"learn_bot_admin_panel/internal/chanel_bus"
+	"learn_bot_admin_panel/internal/transaction"
+	"learn_bot_admin_panel/rimport"
+	"learn_bot_admin_panel/tools/logger"
+	"learn_bot_admin_panel/uimport"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"sync"
+	"time"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/go-telegram/bot"
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
+	fmt.Println("SUCK BOY")
+
+	var wg sync.WaitGroup
+
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
@@ -22,7 +43,96 @@ func main() {
 		Password: config.RedisPass,
 	})
 	defer rdb.Close()
+
 	if _, err := rdb.Ping(ctx).Result(); err != nil {
 		log.Panic("ошибка при пинге redis: ", err)
 	}
+
+	// подключение к postgres
+	pgdb, err := sqlx.Connect("postgres", config.PostgresURL)
+	if err != nil {
+		log.Fatalln("не удалось подключиться к базе postgres: ", err)
+	}
+	defer pgdb.Close()
+
+	if err := pgdb.Ping(); err != nil {
+		log.Fatal("ошибка при пинге postgres : ", err)
+	}
+
+	// инициализация логгера
+	hook := logger.NewPostgresHook(pgdb)
+	logger, err := logger.InitLogger(hook)
+	if err != nil {
+		log.Fatalln("Не удалось инициализировать логгер:", err)
+	}
+
+	// инициализация session manager
+	sessionManager := transaction.NewSQLSessionManager(pgdb)
+
+	// инициализация бота
+	b, err := bot.New(config.BotToken)
+	if err != nil {
+		log.Panic("ошибка при чтении токена бота: ", err)
+	}
+	defer b.Close(ctx)
+
+	// Настройка HTTP-сервера
+	ginConfig := cors.DefaultConfig()
+	ginConfig.AllowAllOrigins = true
+
+	router := gin.Default()
+	router.Use(cors.New(ginConfig))
+
+	srv := &http.Server{
+		Addr:    config.ServerPort,
+		Handler: router,
+	}
+
+	// инициализация event bus авторизация
+	authChan := chanel_bus.NewAuthChan()
+	// инициализация репо
+	ri := rimport.NewRepositoryImports(config, rdb)
+
+	// инициализация usecase
+	ui := uimport.NewUsecaseImport(ri, logger, authChan, config)
+
+	// инициализация rest handler
+	rest_api.NewProfileHandler(ui, router, config)
+
+	// инициализация tg bot handler
+	bot_api.NewBotProfileHandler(ui, b, config, logger, sessionManager)
+
+	wg.Add(2)
+	// запуск gin
+	go func() {
+		defer wg.Done()
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Panic("Ошибка при запуске HTTP-сервера: ", err)
+		}
+	}()
+
+	// запуск api бота
+	go func() {
+		defer wg.Done()
+
+		b.Start(ctx)
+	}()
+
+	// Ожидание сигнала завершения
+	<-ctx.Done()
+	log.Println("Останавливаем приложение...")
+
+	// Даём время на завершение запросов
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	// Остановка HTTP-сервера
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Println("Ошибка при остановке HTTP-сервера:", err)
+	}
+
+	// Ожидание завершения всех горутин
+	wg.Wait()
+	log.Println("Приложение остановлено")
 }
