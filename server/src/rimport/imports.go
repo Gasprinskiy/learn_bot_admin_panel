b142a085e92d5e6e4617f0bc0be9ea4b7f7f0ff7
samgasper <zakirov.ismail@sarkor.uz>
2025-07-28 17:35:18 +0500

    chore: rimport repos

--- server/src/rimport/imports.go
+++ server/src/rimport/imports.go
@@ -4,6 +4,7 @@ import (
 	"learn_bot_admin_panel/config"
 	http_rep "learn_bot_admin_panel/internal/repository/http"
 	"learn_bot_admin_panel/internal/repository/postgres"
+	"learn_bot_admin_panel/internal/repository/redis_cache"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -15,9 +16,9 @@ type RepositoryImports struct {
 func NewRepositoryImports(config *config.Config, rdb *redis.Client) *RepositoryImports {
 	return &RepositoryImports{
 		Repository: Repository{
-			// UserCache: redis_cache.NewUserCache(rdb, config.RedisTtl),
-			Profile: postgres.NewProfile(),
-			TgBot:   http_rep.NewTgBot(config.TgApiURL, config.BotToken),
+			AuthCache: redis_cache.NewAuthCache(rdb, config.SSETTL),
+			Profile:   postgres.NewProfile(),
+			TgBot:     http_rep.NewTgBot(config.TgApiURL, config.BotToken),
 		},
 	}
 }
--- server/src/rimport/repository.go
+++ server/src/rimport/repository.go
@@ -3,6 +3,7 @@ package rimport
 import "learn_bot_admin_panel/internal/repository"
 
 type Repository struct {
-	Profile repository.Profile
-	TgBot   repository.TgBot
+	AuthCache repository.AuthCache
+	Profile   repository.Profile
+	TgBot     repository.TgBot
 }
