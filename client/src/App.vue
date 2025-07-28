dd37d02dc5d400d5b4514c1758c33aeb9a92ee7c
samgasper <zakirov.ismail@sarkor.uz>
2025-07-28 17:57:41 +0500

    feat: init splash screen components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
-
+import { NConfigProvider, NGlobalStyle, NScrollbar, NMessageProvider } from 'naive-ui';
+// import AppPageHeader from '@/components/header/Header.vue';
+// import ModalProvider from '@/components/modal-provier/ModalProvider.vue';
 </script>
 
 <template>
-  <RouterView />
+  <NConfigProvider>
+    <NMessageProvider>
+      <NGlobalStyle />
+
+      <!-- <ModalProvider /> -->
+      <div class="app-wrapper">
+        <!-- <AppPageHeader /> -->
+        <div class="app-wrapper__scrollbar-container">
+          <NScrollbar class="app-wrapper__scrollbar">
+            <RouterView />
+          </NScrollbar>
+        </div>
+      </div>
+    </NMessageProvider>
+  </NConfigProvider>
 </template>
 
-<style scoped>
+<style lang="scss">
+.app-wrapper {
+  &__scrollbar-container {
+    padding: 16px;
+  }
 
+  &__scrollbar {
+    height: calc(100vh - 92px);
+  }
+}
 </style>
